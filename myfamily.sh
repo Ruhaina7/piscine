610ca468e1515c0783cfbc7e83cd6da040ab3b86
ralasfoor <rowiiasfoor@gmail.com>
2023-09-03 19:06:13 +0300

    heb

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
