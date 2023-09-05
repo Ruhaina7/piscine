aa574087379e0beebc532b7719db081502380b35
ralasfoor <rowiiasfoor@gmail.com>
2023-09-05 03:59:19 +0300

    CCC

--- printcomb.go
+++ printcomb.go
@@ -4,22 +4,22 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-		for r := '0'; r <= '7'; r++ {
-			for u := '1'; u <='8'; u++ {
-				for h := '2'; h <= '9'; h++ {
-					
-					if r < u && u < h {
+	for r := '0'; r <= '7'; r++ {
+		for u := '1'; u <= '8'; u++ {
+			for h := '2'; h <= '9'; h++ {
 
-							z01.PrintRune(r)
-							z01.PrintRune(u)
-							z01.PrintRune(h)
+				if r < u && u < h {
 
-							if r != '7' || u != '8' || h != '9' {
+					z01.PrintRune(r)
+					z01.PrintRune(u)
+					z01.PrintRune(h)
 
-									z01.PrintRune(',')
-									z01.PrintRune(' ')
+					if r != '7' || u != '8' || h != '9' {
 
-						}
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+
+					}
 				}
 			}
 		}
