d10921e0b28d571851a774748da22d6e47fa9af0
ralasfoor <rowiiasfoor@gmail.com>
2023-09-05 04:29:47 +0300

    iwannasleeppppp

--- printcomb.go
+++ printcomb.go
@@ -3,26 +3,24 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-
-	for i := '0'; i <= '7'; i++ {
-		for j := '1'; j <= '8'; j++ {
-			for k := '2'; k <= '9'; k++ {
-
-				if i < j && j < k {
-
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(k)
-
-					if i != '7' || j != '8' || k != '9' {
-
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '9'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				if a < b {
+					if b < c {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(c)
+						if a == '7' && b == '8' && c == '9' {
+							z01.PrintRune(10)
+						} else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
+						}
 
 					}
 				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
