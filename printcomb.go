a76d982b105379983e098d5b0f0c1037a39dda42
ralasfoor <rowiiasfoor@gmail.com>
2023-09-05 03:54:01 +0300

    cccccc

--- printcomb.go
+++ printcomb.go
@@ -3,18 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for r := '0'; r <= '7'; r++ {
-		for u := '1'; u <='8'; u++ {
-			for h := '2'; h <= '9'; h++ {
-				if r < u && u < h {
 
-						z01.PrintRune(r)
-						z01.PrintRune(u)
-						z01.PrintRune(h)
+		for r := '0'; r <= '7'; r++ {
+			for u := '1'; u <='8'; u++ {
+				for h := '2'; h <= '9'; h++ {
+					
+					if r < u && u < h {
 
-						if r != '7' || u != '8' || h != '9' {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
+							z01.PrintRune(r)
+							z01.PrintRune(u)
+							z01.PrintRune(h)
+
+							if r != '7' || u != '8' || h != '9' {
+
+									z01.PrintRune(',')
+									z01.PrintRune(' ')
+
 						}
 				}
 			}
