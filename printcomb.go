59177ede6d09659245ad82d1bbdadda29e415d66
ralasfoor <rowiiasfoor@gmail.com>
2023-09-05 04:11:18 +0300

    uhiuhuh

--- printcomb.go
+++ printcomb.go
@@ -6,15 +6,15 @@ func PrintComb() {
 
 	for r := '0'; r <= '7'; r++ {
 		for u := '1'; u <= '8'; u++ {
-			for h := '2'; h <= '9'; h++ {
+			for n := '2'; n <= '9'; n++ {
 
-				if r < u && u < h {
+				if r < u && u < n {
 
 					z01.PrintRune(r)
 					z01.PrintRune(u)
-					z01.PrintRune(h)
+					z01.PrintRune(n)
 
-					if r != '7' || u != '8' || h != '9' {
+					if r != '7' || u != '8' || n != '9' {
 
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
