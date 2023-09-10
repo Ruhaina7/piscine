623db430bdede5b0d09359024bc3aaa442881191
ralasfoor <rowiiasfoor@gmail.com>
2023-09-10 16:26:25 +0300

    ppoipoi

---
+++ isprime.go
@@ -0,0 +1,23 @@
+package piscine
+
+func IsPrime(num int) bool {
+	if num > 0 {
+		if num == 1{
+			return false
+		}
+		if num == 2 || num == 3 {
+			return true
+		}
+		if num%3 == 0 || num%2 == 0 {
+			return false
+		}
+		for r := 2; r < num; r++ {
+			if num%r == 0 {
+				return false
+			}
+		}
+		return true
+	} else {
+		return false
+	}
+}
