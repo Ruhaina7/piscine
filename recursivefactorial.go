33963af671f63f002140f87ced85a9265fe5b1fd
ralasfoor <rowiiasfoor@gmail.com>
2023-09-10 13:52:00 +0300

    ujiujij

---
+++ recursivefactorial.go
@@ -0,0 +1,12 @@
+package piscine
+
+func RecursiveFactorial(num int) int {
+	if num == 0 {
+		return 1
+	}
+	if num < 0 || num > 100 {
+		return 0
+	} else (
+		return num * RecursiveFactorial(num-1)
+	)
+}
