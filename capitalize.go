74f90097a580c37d628db995e03097e25631f709
ralasfoor <rowiiasfoor@gmail.com>
2023-09-12 00:56:19 +0300

    uuuuuuu

--- capitalize.go
+++ capitalize.go
@@ -1,19 +1,26 @@
 package piscine
 
 func Capitalize(s string) string {
-	runes := []rune(s)
-	isFirstLetter := true
-	for i := range runes {
-		if runes[i] >= 'a' && runes[i] <= 'z' && isFirstLetter {
-			runes[i] -= 32
-			isFirstLetter = false
-		} else if (IsRuneDigit(runes[i]) || (runes[i] >= 'A' && runes[i] <= 'Z')) && isFirstLetter {
-			isFirstLetter = false
-		} else if !isFirstLetter && runes[i] >= 'A' && runes[i] <= 'Z' {
-			runes[i] += 32
-		} else if !isFirstLetter && !IsRuneDigit(runes[i]) && !IsRuneAlpha(runes[i]) {
-			isFirstLetter = true
+	runeArray := []rune(s)
+	for i, char := range runeArray {
+		if isNumberOrAlph(char) {
+			if i == 0 || isNumberOrAlph(runeArray[i-1]) == false {
+				if runeArray[i] >= 'a' && runeArray[i] <= 'z' {
+					runeArray[i] = char - 32
+				}
+			} else {
+				if runeArray[i] >= 'A' && runeArray[i] <= 'Z' {
+					runeArray[i] = char + 32
+				}
+			}
 		}
 	}
-	return string(runes)
+	return string(runeArray)
+}
+
+func isNumberOrAlph(r rune) bool {
+	if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' {
+		return true
+	}
+	return false
 }
