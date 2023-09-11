358abc7cbd210dd58f813f7f1d2d00b6d7ac6d0f
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 15:32:48 +0300

    kurkefni

--- alphacount.go
+++ alphacount.go
@@ -2,7 +2,7 @@ package piscine
 
 func AlphaCount(r string) int {
 	count := 0
-	for _, value := range str {
+	for _, value := range {
 		if (value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z') {
 			count += 1
 		}
