2c139da87556ed0b647fece352737c21bd325f23
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 22:24:32 +0300

    uuuuui

--- index.go
+++ index.go
@@ -1,38 +1,12 @@
 package piscine
 
-func Len(r string) int {
-	count := 0
-	for range r {
-		count += 1
-	}
-	return count
-}
-
-func Index(e string, tof string) int {
-	if tof == "" {
-		return 0
-	}
-	rr := []rune(e)
-	bb := []rune(tof)
-	length := Len(tof)
-	ind := -1
-	count := 0
-	for p := 0; p < length(e); p++ {
-		if rr[p] == bb[count] {
-			if count == 0 {
-				ind = p
-			}
-			count++
-			if count == length {
-				return ind
-			}
-		} else {
-			if count > 0 {
-				p--
-			}
-			count = 0
-			ind = -1
+func Index(r string, toFind string) int {
+	lenr := len(r)
+	lentofind := len(toFind)
+	for u := 0; u <= lenr-lentofind; u++ {
+		if r[u:u+lentofind] == toFind {
+			return i
 		}
 	}
-	return ind
+	return -1
 }
