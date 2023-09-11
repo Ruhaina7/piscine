fb783f24d2c7c3e9c80a114d9dfbb83b94f3d202
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 22:25:31 +0300

    jjjjj

--- index.go
+++ index.go
@@ -5,7 +5,7 @@ func Index(r string, toFind string) int {
 	lentofind := len(toFind)
 	for u := 0; u <= lenr-lentofind; u++ {
 		if r[u:u+lentofind] == toFind {
-			return i
+			return u
 		}
 	}
 	return -1
