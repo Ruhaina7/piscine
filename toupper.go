9bee1116ce3b3b89e6d9e02992ff2dbccfccf844
ralasfoor <rowiiasfoor@gmail.com>
2023-09-12 00:02:04 +0300

    olll

---
+++ toupper.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToUpper(s1 string) string {
+	result := ""
+	for r := 0; r < len(s1); r++ [
+		u := s1[r]
+		if u >= 'a' && u <= 'z' {
+			u = u - ('a' - 'A')
+		}
+		result += string(u)
+	]
+	return result
+}
