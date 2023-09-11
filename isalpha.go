fceae0df463feed125e4c22c8aa98a02352ec78a
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 23:08:49 +0300

    fg

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s1 string) bool {
 	r := []rune(s1)
-	for u := 0; u < len(u); u++ {
+	for u := 0; u < len(r); u++ {
 		if (r[u] < 'a' || r[u] > 'z') && (r[u] < 'A' || r[u] > 'Z') && (r[u] < '0' || r[u] > '9') {
 			return false
 		}
