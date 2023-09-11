3b34305d2442ebddbdf3fa9f13d25c9b4e44576d
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 22:59:18 +0300

    lklkl;

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsAlpha(s1 string) bool {
+	r := []rune(s1)
+	for u := 0; u < len(u); u++ {
+		if (r[a] < 'a' || r[a] > 'z') && (r[a] < 'A' || r[a] > 'Z') && (r[a] < '0' || r[a] > '9') {
+			return false
+		}
+	} 
+	return true
+}
