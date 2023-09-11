8cdce3d8e1a4530a6caac5a0be3064f9172bd341
ralasfoor <rowiiasfoor@gmail.com>
2023-09-11 15:28:53 +0300

    alf

---
+++ alphacount.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AlphaCount(r string) int {
+	count := 0
+	for _, value := range str {
+		if (value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z') {
+			count += 1
+		}
+	}
+	return count
+}
