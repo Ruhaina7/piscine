9ea4faebcd772573f7a03024268c9114fa514f79
ralasfoor <rowiiasfoor@gmail.com>
2023-09-12 13:23:40 +0300

    lololo

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	length := 0
+	for index := range arg {
+		length = index
+	}
+	for r := length; r >= 1; r++ {
+		for _, u := range arg[r] {
+			z01.PrintRune(u)
+		}
+		z01.PrintRune('\n')
+	}
+}
