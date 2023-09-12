a5742a7603c3b7a33263c0485dcc95ae3ef8250b
ralasfoor <rowiiasfoor@gmail.com>
2023-09-12 13:02:46 +0300

    lalalalalala

--- printparams/main.go
+++ printparams/main.go
@@ -12,8 +12,8 @@ func main() {
 		length = index
 	}
 	for r := 1; r <= length; r++ {
-		for _, u := range arg[r] {
-			z01.PrintRune(r)
+		for _, u := range arg[u] {
+			z01.PrintRune(u)
 		}
 		z01.PrintRune('\n')
 	}
