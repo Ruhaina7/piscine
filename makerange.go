4c278df7156a2388f878fb316bdb6753c41024a3
ralasfoor <rowiiasfoor@gmail.com>
2023-09-17 17:47:29 +0300

    makemake

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if max <= min {
+		return nil
+	}
+	var result2 []int = make([int, max-min])
+	for r, u := min, 0; r < max; r++{
+		result2[u] = r
+		u++
+	}
+}
