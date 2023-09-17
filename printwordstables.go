25d28d457b964b1531d2e25f0295915e05b4ee3b
ralasfoor <rowiiasfoor@gmail.com>
2023-09-17 20:10:19 +0300

    jhkhgjhgjjhg

---
+++ printwordstables.go
@@ -0,0 +1,12 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(r []string) {
+	for _, str1 := range r {
+		for _, ch := range str1 {
+				z01.PrintRune(ch)
+		}
+	}
+	z01.PrintRune('\n')
+}
