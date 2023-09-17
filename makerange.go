b377bc7c25ce02237ba4f452bf800aff0e32301a
ralasfoor <rowiiasfoor@gmail.com>
2023-09-17 17:48:51 +0300

    yyyy

--- makerange.go
+++ makerange.go
@@ -4,7 +4,7 @@ func MakeRange(min, max int) []int {
 	if max <= min {
 		return nil
 	}
-	var result2 []int = make([int, max-min])
+	var result2 []int = make([]int, max-min)
 	for r, u := min, 0; r < max; r++{
 		result2[u] = r
 		u++
