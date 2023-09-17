d1d3cdadc08b76a76e18466ce1fe4028a37ec42b
ralasfoor <rowiiasfoor@gmail.com>
2023-09-17 18:11:55 +0300

    kkkkmeow

--- concatparams.go
+++ concatparams.go
@@ -1,17 +1,17 @@
 package piscine
 
 func ConcatParams(args []string) string {
-	var res1ult1 string
+	var result string
 	size := 0
 	for r := range args {
 		r++
 		size++
 	}
 	for r, t := range args {
-		res1ult1 += t
+		res1ult += t
 		if r != size-1 {
-			result1 += "\n"
+			result += "\n"
 		}
 	}
-	return res1ult1
+	return result
 }
