6d789011ccbbc21177c8d23a471f574a2df2a076
ralasfoor <rowiiasfoor@gmail.com>
2023-09-17 20:38:19 +0300

    kkkj

---
+++ boolean/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	if isEven(lengthOfArg) == 1 {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
