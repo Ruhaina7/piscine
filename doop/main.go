7bf4201d19eb08b33f2c035c47a5e66e9f47d10b
ralasfoor <rowiiasfoor@gmail.com>
2023-09-19 10:53:06 +0300

    kllk

--- doop/main.go
+++ doop/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -16,30 +16,30 @@ func validateOperator(test string) bool {
 	return false
 }
 
-func main(){
+func main() {
 	args := os.Args[1:]
 	if len(args) > 3 || len(args) < 3 {
 		fmt.Print()
-	}else{
+	} else {
 		if validateOperator(args[1]) == false {
 			fmt.Println(0)
-		}else{
+		} else {
 			premier, _ := strconv.Atoi(args[0])
 			second, _ := strconv.Atoi(args[2])
 			
 			if args[1] == "%" && second == 0 {
 				fmt.Print("No Modulo by 0\n")
-			}else if args[1] == "/" && second == 0 {
+			} else if args[1] == "/" && second == 0 {
 				fmt.Print("No division by 0\n")
-			}else if args[1] == "+" {
+			} else if args[1] == "+" {
 				fmt.Println(premier+second)
-			}else if args[1] == "-" {
+			} else if args[1] == "-" {
 				fmt.Println(premier-second)
-			}else if args[1] == "*" {
+			} else if args[1] == "*" {
 				fmt.Println(premier * second)
-			}else if args[1] == "/" {
+			} else if args[1] == "/" {
 				fmt.Println(premier/second)
-			}else{
+			} else {
 				fmt.Println(premier%second)
 			}
 
