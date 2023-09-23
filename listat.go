a840152fdbd0e3c130f224c2613234bc05f58721
ralasfoor <rowiiasfoor@gmail.com>
2023-09-23 21:36:19 +0300

    atatat

---
+++ listat.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ListAt(l *NodeL, pos int) *NodeL {
+
+	iterator := l
+	inc := 0
+	for iterator != nil {
+		if pos == inc {
+			return iterator
+		}
+		inc++
+		iterator = iterator.Next
+	}
+	return nil
+}
