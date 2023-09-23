34e5d29e697244b88fd1c61af8b8483341938a0e
ralasfoor <rowiiasfoor@gmail.com>
2023-09-23 21:20:24 +0300

    hjhj

--- listlast.go
+++ listlast.go
@@ -1,16 +1,15 @@
-
 package piscine
 
 func ListLast(l *List) interface{} {
-	
-if l.Head == nil {
-	return nil
-}
-for l.Head != nil {
-	if l.Head.Next == nil {
-		return l.Head.Data
+
+	if l.Head == nil {
+		return nil
 	}
-	l.Head = l.Head.Next
-}
-return l.Head.Data
+	for l.Head != nil {
+		if l.Head.Next == nil {
+			return l.Head.Data
+		}
+		l.Head = l.Head.Next
+	}
+	return l.Head.Data
 }
